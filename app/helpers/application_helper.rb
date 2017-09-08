71a0c5c8a1d0d36b9bc0fe3800408156abcbfc65
shinukai <ukai.ynu@gmail.com>
2017-09-08 06:14:49 +0000

    title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def full_title (page_title="")
+    base_title = "Rails fourthapp" #基本のタイトル
+    if page_title.empty?            #ページタイトルが空なら
+      base_title                  #基本タイトル表示
+    else                          #もしくは
+      base_title+"|"+page_title            #基本＋ページタイトル
+    end
+  end
 end
