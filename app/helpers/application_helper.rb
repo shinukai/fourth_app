0986def4148286c8f0b628280089da726768ee2f
shinukai <ukai.ynu@gmail.com>
2017-09-14 05:51:19 +0000

    change appname

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   
   def full_title (page_title="")
-    base_title = "Only View" #基本のタイトル
+    base_title = "Otwitter"       #基本のタイトル
     if page_title.empty?            #ページタイトルが空なら
       base_title                  #基本タイトル表示
     else                          #もしくは
